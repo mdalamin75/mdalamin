c97a7d4b8197fed4c13833dbf48d2296bb61255b
MD.AL-AMIN <mdalmain67378@gmail.com>
2025-02-18 12:05:35 +0600

    update sitemap

--- pages/sitemap.js
+++ pages/sitemap.js
@@ -4,27 +4,19 @@ export default function sitemap() {
     return [
       {
         url: 'https://mdalamin.vercel.app',
-        lastModified: new Date().toISOString(), // Convert Date to ISO string
-        changeFrequency: 'yearly', // Note: Next.js doesn't actually use this value
-        priority: 1, // Note: Next.js doesn't actually use this value
+        lastModified: new Date().toISOString(),
       },
       {
         url: 'https://mdalamin.vercel.app/about',
         lastModified: new Date().toISOString(),
-        changeFrequency: 'yearly',
-        priority: 0.9,
       },
       {
         url: 'https://mdalamin.vercel.app/portfolio',
         lastModified: new Date().toISOString(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
       },
       {
         url: 'https://mdalamin.vercel.app/contact',
         lastModified: new Date().toISOString(),
-        changeFrequency: 'yearly',
-        priority: 0.7,
       },
     ];
   }
